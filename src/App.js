fd3dcf2c61ab4912b8e09fb9115811d4af08eeb9
Jo3o5itor <joao.guimaraes741@gmail.com>
2025-04-08 10:45:29 -0300

    fix: new settings add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+// Importa o React
 import React from 'react';
+// Importa o componente que exibe a lista de boxeadores
 import BoxeadorList from './components/BoxeadorList';
 
 const App = () => {
   return (
+    // Renderiza o componente BoxeadorList dentro da aplicação
     <div>
       <BoxeadorList />
     </div>
   );
 };
 
-export default App;
+export default App; // Exporta o componente App como padrão
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
-// api.js
+// Importa a biblioteca axios para fazer requisições HTTP
 import axios from 'axios';
 
+// Cria uma instância personalizada do axios com a base da URL da API
 const api = axios.create({
-  baseURL: 'https://hajime-no-ippo-api.onrender.com/ts/api/v1/boxers',
+  baseURL: 'https://hajime-no-ippo-api.onrender.com/ts/api/v1/boxers', // URL base da API de boxeadores
 });
 
-export default api;
+export default api; // Exporta a instância para ser usada em outros arquivos
